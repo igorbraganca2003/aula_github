844ad4504b0a03ed13d8a86aa407395ce3139c53
Igor <igor.braganca2003@gmail.com>
2023-06-29 16:20:33 -0300

    Initial Commit

---
+++ aula-github/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  aula-github
+//
+//  Created by Igor Bragança Toledo on 29/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aula-github/aula_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  aula_githubApp.swift
+//  aula-github
+//
+//  Created by Igor Bragança Toledo on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct aula_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
